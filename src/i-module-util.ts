1f087560d5d636d86318bf5e38f23e648621e906
wessberg <frederikwessberg@gmail.com>
2017-08-18 03:16:07 +0200

    Added a missing typing

--- src/i-module-util.ts
+++ src/i-module-util.ts
@@ -2,5 +2,5 @@ import {InsertionOrderedSet} from "@wessberg/insertion-ordered-set";
 
 export interface IModuleUtil {
 	resolvePath (filePath: string, from?: string): string;
-	builtInModules: InsertionOrderedSet;
+	builtInModules: InsertionOrderedSet<string>;
 }
